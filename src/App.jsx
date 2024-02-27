f86520260980cf09af06cf45de10ca6f285867f7
Yasin İlkyaz <79060926+yasinilkyaz@users.noreply.github.com>
2024-02-27 20:07:02 +0300

    proje eklendi

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import questions from './questions'; // questions.js dosyasını dahil edin
+
+function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState('');
+  const [showOptions, setShowOptions] = useState(false);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [score, setScore] = useState(0);
+  const [wrongAnswers, setWrongAnswers] = useState(0);
+  const [timer, setTimer] = useState(30);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [showResults, setShowResults] = useState(false); // Sonuçların gösterilip gösterilmeyeceğini izleyen durum
+
+  useEffect(() => {
+    if (quizStarted) {
+      const interval = setInterval(() => {
+        setTimer(prevTimer => {
+          if (prevTimer > 0) {
+            return prevTimer - 1;
+          } else {
+            return 0; // Negatif sayıya düşmesini önle
+          }
+        });
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }
+  }, [quizStarted]);
+  useEffect(() => {
+    if (timer === 20) {
+      setShowOptions(true);
+    } else if (timer === 0 && !isAnswered) {
+      handleNextQuestion();
+    }
+  }, [timer, isAnswered]);
+
+  const handleOptionClick = (option) => {
+    if (!isAnswered) {
+      setSelectedOption(option);
+      setIsAnswered(true);
+
+      if (option === questions[currentQuestion].answer) {
+        setScore(score + 1);
+      } else {
+        setWrongAnswers(wrongAnswers + 1);
+      }
+    }
+  };
+
+  const handleNextQuestion = () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+      setSelectedOption('');
+      setShowOptions(false);
+      setIsAnswered(false);
+      setTimer(30);
+    } else {
+      setShowResults(true); // Son soruda sonuçları göster
+    }
+  };
+
+  const handleStartQuiz = () => {
+    setQuizStarted(true);
+    setTimer(30); // Test başladığında timer'ı 30 saniyeye ayarla
+  };
+
+  return (
+    <div className="app">
+      {!quizStarted && (
+        <div className="start-container">
+          <h2>Teste Hoş Geldiniz!</h2>
+          <p>Teste başlamak için lütfen aşağıdaki butona tıklayın.</p>
+          <button onClick={handleStartQuiz} id="start">Teste Başla</button>
+        </div>
+      )}
+      {quizStarted && !showResults && (
+        <div className="question-container">
+          <h2>{questions[currentQuestion].question}</h2>
+          {questions[currentQuestion].media && (
+            <img src={questions[currentQuestion].media} alt="media" />
+          )}
+          {showOptions && (
+            <div className="options-container">
+              {questions[currentQuestion].options.map((option, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleOptionClick(option)}
+                  className={`option ${selectedOption === option && 'selected'}`}
+                  disabled={isAnswered}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          )}
+          <div className="timer">
+            <p>{timer} sn</p>
+          </div>
+        </div>
+      )}
+      <div className="button-container">
+        {quizStarted && !showResults && (
+          <button onClick={handleNextQuestion} id="start">
+            {currentQuestion === questions.length - 1 ? 'Sonuçları Gör' : 'Sonraki Soru'}
+          </button>
+        )}
+      </div>
+      <div className="score-container">
+        {showResults && (
+          <div>
+            <p>Doğru Sayısı: {score}</p>
+            <p>Yanlış Sayısı: {wrongAnswers}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/questions.js
@@ -0,0 +1,73 @@
+const questions = [
+  {
+    question: "Çin Seddini oluşturan taşlar birbirine ne ile tutturulmuştur?",
+    options: ["Bambu Harcı", "Anne Duası", "Pirinç Unu", "Noodle"],
+    answer: "Pirinç Unu",
+    media: "../public/pictures/cin-seddi.jpg",
+  },
+  {
+    question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
+    options: ["Gıda Mühendisi", "Diş Doktoru", "Ev Hanımı", "Güzellik Uzmanı"],
+    answer: "Diş Doktoru",
+    media: "../public/pictures/pamuk.jpg",
+  },
+  {
+    question:
+      "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamda bulunan kişi kimdir?",
+    options: ["Gülben Ergen", "Hülya Avşar", "Harika Avcı", "Sevtap Parman"],
+    answer: "Gülben Ergen",
+    media: "../public/pictures/tarkan.jpg",
+  },
+  {
+    question: "Pteronofobi nedir?",
+    options: [
+      "Yeşil ışık yanar yanmaz korna çalacak korkusu",
+      "Fakir kalma korkusu",
+      "Taksi bulamama korkusu",
+      "Kuş tüyüyle gıdıklanma korkusu",
+    ],
+    answer: "Kuş tüyüyle gıdıklanma korkusu",
+    media: "../public/pictures/fobi.jpg",
+  },
+  {
+    question:
+      "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
+    options: ["43", "78", "23", "99"],
+    answer: "43",
+    media: "../public/pictures/balik.jpg",
+  },
+  {
+    question:
+      "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
+    options: ["Sezen Aksu", "Sibel Can", "Mustafa Sandal", "Bülent Ersoy"],
+    answer: "Mustafa Sandal",
+    media: "../public/pictures/bandira.jpg",
+  },
+  {
+    question:
+      "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
+    options: ["Hande Yener", "Hadise", "Gülşen", "Simge"],
+    answer: "Hadise",
+    media: "../public/pictures/billboard.jpg",
+  },
+  {
+    question: "Antik Roma'da kadınlar parfüm olarak ne kullanıyordu?",
+    options: ["Gül Suyu", "Bal", "Gladyatör Teri", "Kan"],
+    answer: "Gladyatör Teri",
+    media: "../public/pictures/parfum.jpg",
+  },
+  {
+    question: "T-Rex'in yaşayan en yakın akrabası aşağıdakilerden hangisidir?",
+    options: ["İnekler", "Tavuklar", "Timsahlar", "Köpekler"],
+    answer: "Tavuklar",
+    media: "../public/pictures/trex.jpg",
+  },
+  {
+    question:
+      "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
+    options: ["Çerofobi", "Euphobia", "Felicifobia", "Mutluluk Korkusu"],
+    answer: "Çerofobi",
+    media: "../public/pictures/fobi.jpg",
+  },
+];
+export default questions
