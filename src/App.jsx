f8ab03180e13320562549ebbc92ef84c0b394b7c
Yasin İlkyaz <79060926+yasinilkyaz@users.noreply.github.com>
2024-02-29 04:17:46 +0300

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import questions from './questions';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import questions from "./questions";
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
   const [showOptions, setShowOptions] = useState(false);
   const [isAnswered, setIsAnswered] = useState(false);
   const [score, setScore] = useState(0);
   const [wrongAnswers, setWrongAnswers] = useState(0);
   const [timer, setTimer] = useState(30);
   const [quizStarted, setQuizStarted] = useState(false);
-  const [showResults, setShowResults] = useState(false); 
+  const [showResults, setShowResults] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState(
+    Array(questions.length).fill("")
+  );
+
   useEffect(() => {
     if (quizStarted) {
       const interval = setInterval(() => {
-        setTimer(prevTimer => {
+        setTimer((prevTimer) => {
           if (prevTimer > 0) {
             return prevTimer - 1;
           } else {
-            return 0; 
+            return 0;
           }
         });
       }, 1000);
-  
+
       return () => clearInterval(interval);
     }
   }, [quizStarted]);
   useEffect(() => {
     if (timer === 20) {
       setShowOptions(true);
-    } else if (timer === 0 ) {
+    } else if (timer === 0) {
       handleNextQuestion();
     }
   }, [timer, isAnswered]);
 
+  useEffect(() => {
+    if (timer === 0 && !isAnswered) {
+      const correctAnswer = questions[currentQuestion].answer;
+      setSelectedOptions((prevSelectedOptions) => {
+        const updatedSelectedOptions = [...prevSelectedOptions];
+        updatedSelectedOptions[currentQuestion] = correctAnswer;
+        return updatedSelectedOptions;
+      });
+      setIsAnswered(true);
+      setWrongAnswers(wrongAnswers + 1);
+    }
+  }, [timer]);
+
   const handleOptionClick = (option) => {
     if (!isAnswered) {
+      const correctAnswer = questions[currentQuestion].answer;
       setSelectedOption(option);
       setIsAnswered(true);
 
-      if (option === questions[currentQuestion].answer) {
+      if (option === correctAnswer) {
         setScore(score + 1);
       } else {
         setWrongAnswers(wrongAnswers + 1);
       }
+
+      const updatedSelectedOptions = [...selectedOptions];
+      updatedSelectedOptions[currentQuestion] = option;
+      setSelectedOptions(updatedSelectedOptions);
     }
   };
 
   const handleNextQuestion = () => {
     if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
-      setSelectedOption('');
+      setSelectedOption("");
       setShowOptions(false);
       setIsAnswered(false);
       setTimer(30);
@@ -71,7 +93,9 @@ function App() {
         <div className="start-container">
           <h2>Teste Hoş Geldiniz!</h2>
           <p>Teste başlamak için lütfen aşağıdaki butona tıklayın.</p>
-          <button onClick={handleStartQuiz} id="start">Teste Başla</button>
+          <button onClick={handleStartQuiz} id="start">
+            Teste Başla
+          </button>
         </div>
       )}
       {quizStarted && !showResults && (
@@ -86,7 +110,9 @@ function App() {
                 <button
                   key={index}
                   onClick={() => handleOptionClick(option)}
-                  className={`option ${selectedOption === option && 'selected'}`}
+                  className={`option ${
+                    selectedOption === option && "selected"
+                  }`}
                   disabled={isAnswered}
                 >
                   {option}
@@ -102,15 +128,29 @@ function App() {
       <div className="button-container">
         {quizStarted && !showResults && (
           <button onClick={handleNextQuestion} id="start">
-            {currentQuestion === questions.length - 1 ? 'Sonuçları Gör' : 'Sonraki Soru'}
+            {currentQuestion === questions.length - 1
+              ? "Sonuçları Gör"
+              : "Sonraki Soru"}
           </button>
         )}
       </div>
       <div className="score-container">
         {showResults && (
           <div>
-            <p>Doğru Sayısı: {score}</p>
-            <p>Yanlış Sayısı: {wrongAnswers}</p>
+            <p>Doğru Sayısı: {score}</p> <br />
+            <p>Yanlış Sayısı: {wrongAnswers}</p> <br />
+            <div className="selected-options">
+              <h3>Seçilen Seçenekler:</h3>
+              <ul>
+                {questions.map((question, index) => (
+                  <li key={index}>
+                    <strong>{question.question}</strong> <br />
+                    {"Cevabınız"}:{selectedOptions[index]} {" Doğru Cevap :  "}
+                    {question.answer}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         )}
       </div>
